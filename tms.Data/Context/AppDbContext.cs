8dab2e947648e445b170d5d0dbf2eec626fb7efe
Zamin Alili <zaminalili29@gmail.com>
2023-04-03 08:47:08 +0400

    Add db context class

---
+++ tms.Data/Context/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using tms.Entity.Entities;
+
+namespace tms.Data.Context
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        DbSet<Product> Products { get; set; }
+        DbSet<Category> Category { get; set; }
+        DbSet<Image> Images { get; set; }
+        DbSet<Offer> Offers { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
