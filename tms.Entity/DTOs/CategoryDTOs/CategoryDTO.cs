a10cafea1e6a9b82ce06b0f21d67b3602e699157
Zamin Alili <zaminalili29@gmail.com>
2023-04-04 10:28:01 +0400

    Add CategoryDto

---
+++ tms.Entity/DTOs/CategoryDTOs/CategoryDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace tms.Entity.DTOs.CategoryDTOs
+{
+    public class CategoryDTO
+    {
+        public string Name_AZ { get; set; }
+        public string CretedBy { get; set; }
+        public string? EditedBy { get; set; }
+        public string? DeletedBy { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ tms.Service/AutoMapper/Categories/CategoryProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using tms.Entity.DTOs.CategoryDTOs;
+using tms.Entity.Entities;
+
+namespace tms.Service.AutoMapper.Categories
+{
+    public class CategoryProfile: Profile
+    {
+        public CategoryProfile()
+        {
+            CreateMap<CategoryDTO, Category>().ReverseMap();
+        }
+    }
+}
