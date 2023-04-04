149919fdf317c7efa630464850f8391d0aa7bc4c
Zamin Alili <zaminalili29@gmail.com>
2023-04-04 10:42:59 +0400

    Add service layer configs

---
+++ tms.Service/Extensions/ServiceLayerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using AutoMapper;
+using System.Reflection;
+using tms.Service.Services;
+using tms.Service.Services.Abstract;
+using tms.Service.Services.Concrete;
+
+namespace tms.Service.Extensions
+{
+    public static class ServiceLayerExtensions
+    {
+        public static IServiceCollection LoadServiceLayerExtension(this IServiceCollection services)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+
+
+            services.AddAutoMapper(assembly);
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            return services;
+        }
+    }
+}
--- tms.Web/Program.cs
+++ tms.Web/Program.cs
@@ -1,4 +1,5 @@
 using tms.Data.Extensions;
+using tms.Service.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 builder.Services.LoadDataLayerExtensions(builder.Configuration);
+builder.Services.LoadServiceLayerExtension();
 
 var app = builder.Build();
 
