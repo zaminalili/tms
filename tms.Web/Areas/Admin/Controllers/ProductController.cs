951ab61aeb6c0564a5a14088fd6ad34b7ce5a824
Zamin Alili <zaminalili29@gmail.com>
2023-04-08 12:24:55 +0400

    Test product service

---
+++ tms.Web/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using tms.Service.Services.Abstract;
+
+namespace tms.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize]
+    public class ProductController : Controller
+    {
+        private readonly IProductService productService;
+        public ProductController(IProductService productService)
+        {
+            this.productService = productService;
+        }
+        public async Task<IActionResult> Index()
+        {
+           var products = await productService.GetAllProductsAsync();
+
+            return View(products);
+        }
+    }
+}
